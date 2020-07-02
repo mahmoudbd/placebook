c607db5d5bfdd2d2571094c47a2dfa83c586122a
Mahmoud Badran <mahmoudbd@gmail.com>
2020-07-02 02:36:13 +0200

    second

--- app.js
+++ app.js
@@ -19,19 +19,19 @@ const app = express();
 app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use(express.static(path.join('public')));
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// app.use((req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-	next();
-});
+// 	next();
+// });
 
 app.use('/api/socialMedia', socialAuthRoutes);
 app.use('/api/places', placesRoutes);
