21f2b215a2d4f3456a4fca2308c9f3d59e89a367
Mahmoud Badran <mahmoudbd@gmail.com>
2020-07-02 02:28:52 +0200

    delete files

--- frontend/src/shared/context/search-context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export const SearchContext = createContext({
-  searchState: {},
-  inputHandler: () => {},
-  currentPage: 1,
-  setCurrentPage: () => {},
-});
